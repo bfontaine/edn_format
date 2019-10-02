338f19488f0a63fd4f99280b665210a034b08a2b
Baptiste Fontaine <b@ptistefontaine.fr>
2019-10-02 21:11:06 +0200

    dumper class wip

--- edn_format/edn_dump.py
+++ edn_format/edn_dump.py
@@ -55,83 +55,92 @@ def unicode_escape(string):
         return ESCAPE_DCT[match.group(0)]
     return '"' + ESCAPE.sub(replace, string) + '"'
 
+class Dumper(object):
+    def __init__(self,
+            string_encoding=DEFAULT_INPUT_ENCODING,
+            output_encoding=None,
+            keyword_keys=False,
+            sort_keys=False,
+            sort_sets=False,
+            pretty=False):
 
-def seq(obj, **kwargs):
-    return ' '.join([udump(i, **kwargs) for i in obj])
+        self.string_encoding = string_encoding
+        self.output_encoding = output_encoding
+        self.keyword_keys = keyword_keys
+        self.sort_keys = sort_keys
+        self.sort_sets = sort_sets
+        self.pretty = pretty
+        self._indent = 
 
+    def _dump_seq(self, objs):
+        return ' '.join([self._dump(obj) for obj in objs])
 
-def udump(obj,
-          string_encoding=DEFAULT_INPUT_ENCODING,
-          keyword_keys=False,
-          sort_keys=False,
-          sort_sets=False):
+    def _dump(self, obj):
+        if obj is None:
+            return 'nil'
+        if isinstance(obj, bool):
+            return 'true' if obj else 'false'
+        if isinstance(obj, (int, long, float)):
+            return unicode(obj)
+        if isinstance(obj, decimal.Decimal):
+            return '{}M'.format(obj)
+        if isinstance(obj, (Keyword, Symbol)):
+            return unicode(obj)
+        # CAVEAT LECTOR! In Python 3 'basestring' is alised to 'str' above.
+        # Furthermore, in Python 2 bytes is an instance of 'str'/'basestring' while
+        # in Python 3 it is not.
+        if isinstance(obj, bytes):
+            return unicode_escape(obj.decode(self.string_encoding))
+        if isinstance(obj, basestring):
+            return unicode_escape(obj)
+        if isinstance(obj, tuple):
+            return '({})'.format(self._dump_seq(obj))
+        if isinstance(obj, (list, ImmutableList)):
+            return '[{}]'.format(self._dump_seq(obj))
+        if isinstance(obj, set) or isinstance(obj, frozenset):
+            if self.sort_sets:
+                obj = sorted(obj)
+            return '#{{{}}}'.format(self._dump_seq(obj))
+        if isinstance(obj, dict) or isinstance(obj, ImmutableDict):
+            pairs = obj.items()
+            if self.sort_keys:
+                pairs = sorted(pairs, key=lambda p: str(p[0]))
+            if self.keyword_keys:
+                pairs = ((Keyword(k) if isinstance(k, (bytes, basestring)) else k, v) for k, v in pairs)
 
-    kwargs = {
-        "string_encoding": string_encoding,
-        "keyword_keys": keyword_keys,
-        "sort_keys": sort_keys,
-        "sort_sets": sort_sets,
-    }
+            return '{{{}}}'.format(self._dump_seq(itertools.chain.from_iterable(pairs)))
+        if isinstance(obj, fractions.Fraction):
+            return '{}/{}'.format(obj.numerator, obj.denominator)
+        if isinstance(obj, datetime.datetime):
+            return '#inst "{}"'.format(pyrfc3339.generate(obj, microseconds=True))
+        if isinstance(obj, datetime.date):
+            return '#inst "{}"'.format(obj.isoformat())
+        if isinstance(obj, uuid.UUID):
+            return '#uuid "{}"'.format(obj)
+        if isinstance(obj, TaggedElement):
+            return unicode(obj)
+        raise NotImplementedError(
+            u"encountered object of type '{}' for which no known encoding is available: {}".format(
+                type(obj), repr(obj)))
 
-    if obj is None:
-        return 'nil'
-    elif isinstance(obj, bool):
-        return 'true' if obj else 'false'
-    elif isinstance(obj, (int, long, float)):
-        return unicode(obj)
-    elif isinstance(obj, decimal.Decimal):
-        return '{}M'.format(obj)
-    elif isinstance(obj, (Keyword, Symbol)):
-        return unicode(obj)
-    # CAVEAT LECTOR! In Python 3 'basestring' is alised to 'str' above.
-    # Furthermore, in Python 2 bytes is an instance of 'str'/'basestring' while
-    # in Python 3 it is not.
-    elif isinstance(obj, bytes):
-        return unicode_escape(obj.decode(string_encoding))
-    elif isinstance(obj, basestring):
-        return unicode_escape(obj)
-    elif isinstance(obj, tuple):
-        return '({})'.format(seq(obj, **kwargs))
-    elif isinstance(obj, (list, ImmutableList)):
-        return '[{}]'.format(seq(obj, **kwargs))
-    elif isinstance(obj, set) or isinstance(obj, frozenset):
-        if sort_sets:
-            obj = sorted(obj)
-        return '#{{{}}}'.format(seq(obj, **kwargs))
-    elif isinstance(obj, dict) or isinstance(obj, ImmutableDict):
-        pairs = obj.items()
-        if sort_keys:
-            pairs = sorted(pairs, key=lambda p: str(p[0]))
-        if keyword_keys:
-            pairs = ((Keyword(k) if isinstance(k, (bytes, basestring)) else k, v) for k, v in pairs)
-
-        return '{{{}}}'.format(seq(itertools.chain.from_iterable(pairs), **kwargs))
-    elif isinstance(obj, fractions.Fraction):
-        return '{}/{}'.format(obj.numerator, obj.denominator)
-    elif isinstance(obj, datetime.datetime):
-        return '#inst "{}"'.format(pyrfc3339.generate(obj, microseconds=True))
-    elif isinstance(obj, datetime.date):
-        return '#inst "{}"'.format(obj.isoformat())
-    elif isinstance(obj, uuid.UUID):
-        return '#uuid "{}"'.format(obj)
-    elif isinstance(obj, TaggedElement):
-        return unicode(obj)
-    raise NotImplementedError(
-        u"encountered object of type '{}' for which no known encoding is available: {}".format(
-            type(obj), repr(obj)))
-
+    def dump(self, obj):
+        s = self._dump(obj)
+        if self.output_encoding:
+            s = s.encode(self.output_encoding)
+        return s
 
 def dump(obj,
          string_encoding=DEFAULT_INPUT_ENCODING,
          output_encoding=DEFAULT_OUTPUT_ENCODING,
          keyword_keys=False,
          sort_keys=False,
-         sort_sets=False):
-    outcome = udump(obj,
+         sort_sets=False,
+         pretty=False):
+    dumper = Dumper(output_encoding=None if __PY3 else output_encoding,
                     string_encoding=string_encoding,
                     keyword_keys=keyword_keys,
                     sort_keys=sort_keys,
-                    sort_sets=sort_sets)
-    if __PY3:
-        return outcome
-    return outcome.encode(output_encoding)
+                    sort_sets=sort_sets,
+                    pretty=pretty)
+
+    return dumper.dump(obj)
